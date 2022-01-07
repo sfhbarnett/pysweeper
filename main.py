1f0ab5712bf4dafc534dc6f218ca6878ef0f683f
sbarnett <sfhbarnett91@gmail.com>
2022-01-07 10:04:42 +0000

    logic fully in place, game essentially works

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ class MineSweeper(QWidget):
     def initUI(self):
 
         grid = self.generate_grid()
-        print(grid)
+        grid[0][0] = 'nm'
+        for line in grid:
+            print(line)
 
         for width in range(10):
             for height in range(10):
                 type = grid[width][height]
+                if type == 'nm':
+                    nMines = self.calculateNumber(grid,width,height)
+                    type = nMines
                 btn = Button('',parent=self,state=type)
 
                 btn.setGeometry(200,150,20,20)
@@ -27,21 +32,37 @@ class MineSweeper(QWidget):
         self.setWindowTitle('MineSweeper')
         self.show()
 
-    def pressed(self):
-        self.sender().setIcon(QIcon('mine.png'))
 
     def generate_grid(self):
+        ##Randomly assign where mines are generated, 10 percent chance
         totalgrid = []
         for w in range(10):
             row = []
             for h in range(10):
                 r = random.random()
-                if r < 0.1:
+                if r < 0.3:
                     row.append('m')
                 else:
                     row.append('nm')
             totalgrid.append(row)
         return totalgrid
+
+    def calculateNumber(self,grid,w,h):
+        number = 0
+        for x in range(-1,2,1):
+            for y in range(-1,2,1):
+                middle = 0
+                xcoord = w+x
+                ycoord = h+y
+                if -1 < xcoord < len(grid[0]) and -1 < ycoord < len(grid[0]): #only works for square grids
+                    location = grid[w+x][h+y]
+                    if x == 0 and y == 0:
+                        middle = 1
+                    if location == 'm' and middle == 0:
+                        number += 1
+        return number
+
+
 
 class Button(QPushButton):
     def __init__(self,Text,parent=None,state=None):
@@ -57,7 +78,7 @@ class Button(QPushButton):
         if self.state == 'm':
             self.sender().setIcon(QIcon('mine.png'))
         else:
-            self.sender().setText('3')
+            self.sender().setText(str(self.state))
 
 
 def main():
