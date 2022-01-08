fa26ff027b36ba0339f0c63d995f0a416b4ab7c4
sbarnett <sfhbarnett91@gmail.com>
2022-01-08 15:56:27 +0000

    Changed floodfill to 4-connectivity rather than 8, fixed bug with flag setting, cleaned up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
 import random
 
+
 class MineSweeper(QWidget):
 
     def __init__(self):
@@ -11,34 +12,33 @@ class MineSweeper(QWidget):
 
     def initUI(self):
 
-        grid,self.buttongrid = self.generate_grid()
+        grid, self.buttongrid = self.generate_grid()
 
         for width in range(10):
             for height in range(10):
                 type = grid[width][height]
                 if type == 'nm':
-                    nMines = self.calculateNumber(grid,width,height)
+                    nMines = self.calculateNumber(grid, width, height)
                     type = nMines
-                btn = Button('',parent=self,state=type,w=width,h=height)
-                btn.setGeometry(200,150,20,20)
-                btn.move(width*20+40,height*20+40)
+                btn = Button('', parent=self, state=type, w=width, h=height)
+                btn.setGeometry(200, 150, 20, 20)
+                btn.move(width*20+40, height*20+40)
                 self.buttongrid[width][height] = btn
 
-        self.Flagbtn = QPushButton('',self)
+        self.Flagbtn = QPushButton('', self)
         self.Flagbtn.setIcon(QIcon('flag.png'))
         self.Flagbtn.setGeometry(80, 10, 20, 20)
         self.Flagbtn.move(80, 10)
         self.Flagbtn.clicked.connect(self.setFlagMode)
 
-        self.setGeometry(300,300,300,300)
+        self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('MineSweeper')
         self.show()
 
         self.flagstate = -1
 
-
     def generate_grid(self):
-        ##Randomly assign where mines are generated, 10 percent chance
+        # Randomly assign where mines are generated, 10 percent chance
         totalgrid = []
         buttongrid = []
         for w in range(10):
@@ -70,19 +70,28 @@ class MineSweeper(QWidget):
                         number += 1
         return number
 
-    def collapse(self,button,w,h):
-        #Performs the floodfill to fill in all zero squares and reveal edge numbers
-        #Need to change so it's 4 connectivity instead of 8
+    def collapse(self, button):
+        # Performs the floodfill to fill in all zero squares and reveal edge numbers
+        # Need to change so it's 4 connectivity instead of 8
         currentW = button.width
         currentH = button.height
-        for x in range(-1,2,1):
-            for y in range(-1,2,1):
-                xcoord = currentW+x
-                ycoord = currentH+y
-                if -1 < xcoord < len(self.buttongrid[0]) and -1 < ycoord < len(self.buttongrid[0]):
-                    btn = self.buttongrid[xcoord][ycoord]
-                    if btn.checked == False:
-                        btn.click()
+        compass = [[-1,0],[1,0],[0,-1],[0,1]]
+        for p in compass:
+            xcoord = currentW + p[0]
+            ycoord = currentH + p[1]
+            if -1 < xcoord < len(self.buttongrid[0]) and -1 < ycoord < len(self.buttongrid[0]):
+                btn = self.buttongrid[xcoord][ycoord]
+                if btn.checked is False:
+                    btn.click()
+
+        # for x in range(-1, 2, 1):
+        #     for y in range(-1, 2, 1):
+        #         xcoord = currentW+x
+        #         ycoord = currentH+y
+        #         if -1 < xcoord < len(self.buttongrid[0]) and -1 < ycoord < len(self.buttongrid[0]):
+        #             btn = self.buttongrid[xcoord][ycoord]
+        #             if btn.checked is False:
+        #                 btn.click()
 
     def setFlagMode(self):
         self.flagstate *= -1
@@ -93,8 +102,8 @@ class MineSweeper(QWidget):
 
 
 class Button(QPushButton):
-    def __init__(self,Text,parent=None,state=None,w=None,h=None):
-        super(Button,self).__init__(parent)
+    def __init__(self, Text, parent=None, state=None, w=None, h=None):
+        super(Button, self).__init__(parent)
         self.parent = parent
         self.setupbutton()
         self.state = state
@@ -112,12 +121,13 @@ class Button(QPushButton):
             if self.state == 'm':
                 self.sender().setIcon(QIcon('mine.png'))
             elif self.state == 0:
-                self.parent.collapse(self, self.width,self.height)
+                self.parent.collapse(self)
                 self.setStyleSheet("background-color: #6a6461")
             else:
                 self.sender().setText(str(self.state))
         elif self.parent.flagstate == 1 and self.flag == True:
             self.checked = False
+            self.flag = False
             self.sender().setIcon(QIcon(''))
         else:
             self.sender().setIcon(QIcon('flag.png'))
